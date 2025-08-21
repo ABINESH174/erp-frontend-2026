618f0914fe76f0ead79790e0b195b1fcbaaab7f4
dhanush <dhanushdhanush3732@gmail.com>
2025-08-21 16:15:25 +0530

    commmit fot first time production

--- frontend/src/Api/AxiosInstance.js
+++ frontend/src/Api/AxiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "http://192.168.137.1:8080/api",
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 AxiosInstance.interceptors.request.use(
